e2fdbfa5b6bfe9ab220c031271880baa4646a12b
Olushola Adedokun <sholadedokun@yahoo.com>
2018-01-02 17:18:59 +0100

    created static public file

--- app.js
+++ app.js
@@ -60,11 +60,6 @@ server.listen(port, function() {
 });
 app.use(cors());
 
-
-// view engine setup
-app.set('views', path.join(__dirname, 'app/views'));
-app.set('view engine', 'ejs');
-
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
